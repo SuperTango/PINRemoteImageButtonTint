e5cb351bd67eeca5b09e9acf03a95e2805a83b21
Alex Tang <altitude@funkware.com>
2017-01-27 09:40:35 -0800

    example, notice button doesn't get an image

--- PINRemoteImageButtonTint/ViewController.swift
+++ PINRemoteImageButtonTint/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import PINRemoteImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+
+    let url = URL(string: "https://placehold.it/300/ff3300")
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.button.pin_setImage(from: url)
+        self.imageView.pin_setImage(from: url)
     }
 
     override func didReceiveMemoryWarning() {
