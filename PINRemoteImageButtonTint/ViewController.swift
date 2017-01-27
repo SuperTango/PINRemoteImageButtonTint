2c0e5c8aa852ef9c9751c57aab2cc7532b791ac9
Alex Tang <altitude@funkware.com>
2017-01-27 09:25:14 -0800

    Initial Commit

---
+++ PINRemoteImageButtonTint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PINRemoteImageButtonTint
+//
+//  Created by Alex Tang on 1/27/17.
+//  Copyright © 2017 Funkware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
